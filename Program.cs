03086e2dae1f09241197d65fb7ee55517c900903
yoshidamegumu <69129511+yoshidamegumu@users.noreply.github.com>
2020-08-03 13:54:46 +0900

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace TEST
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
